4bef2feea44514e531678e486d8982d6562a59b8
Leo6067 <164778126@qq.com>
2024-03-11 10:32:00 +0800

    20240311  base 增加recyclerview

--- app/src/main/java/com/xy/demo/base/MBBaseActivity.kt
+++ app/src/main/java/com/xy/demo/base/MBBaseActivity.kt
@@ -2,12 +2,17 @@ package com.xy.demo.base
 
 import com.xy.demo.BR
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.ViewDataBinding
+import com.blankj.utilcode.util.NetworkUtils
 import com.xy.demo.R
+import com.xy.network.watch.NetworkStateLiveData
+import com.xy.network.watch.NetworkType
 import com.xy.xframework.base.BaseSharePreference
 import com.xy.xframework.base.XBaseActivity
 import com.xy.xframework.base.XBaseViewModel
 import com.xy.xframework.statusBar.StatusBarUtil
+import com.xy.xframework.utils.ToastUtils
 
 abstract class MBBaseActivity<T : ViewDataBinding, VM : XBaseViewModel> : XBaseActivity<T, VM>() {
 
@@ -41,4 +46,35 @@ abstract class MBBaseActivity<T : ViewDataBinding, VM : XBaseViewModel> : XBaseA
         super.onResume()
 //        logger("@AF", "onResume() -> ${this::class.java.simpleName}")
     }
+    
+    
+    
+    override fun initParams() {
+        super.initParams()
+//        var hasNetWork = NetworkUtils.isConnected()
+//
+//        NetworkStateLiveData.observe(this) {
+//            if (it == NetworkType.CONNECT) {
+//                ToastUtils.showShort(getString(R.string.network_connected))
+//                hasNetWork = true
+//            }
+//            if (it == NetworkType.NONE) {
+//                ToastUtils.showShort(getString(R.string.network_disconnected))
+//                hasNetWork = false
+//            }
+//        }
+//
+//
+//        if (hasNetWork) {
+//            if (notNetWorkLin != null) {
+//                notNetWorkLin?.visibility = View.GONE
+//            }
+//        } else {
+//            if (notNetWorkLin != null) {
+//                notNetWorkLin?.visibility = View.VISIBLE
+//            }
+//        }
+    
+    }
+    
 }
